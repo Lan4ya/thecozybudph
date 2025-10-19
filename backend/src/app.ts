1565cb9445e24c79b0cb0d502244efc9ca5b8ee9
isMaya404 <franciajeremiah10@gmail.com>
2025-10-19 20:16:54 +0800

    init backend

---
+++ backend/src/app.ts
@@ -0,0 +1,60 @@
+import "dotenv/config";
+import express from "express";
+import path from "path";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import { initDB } from "./db/supabase.ts";
+
+const app = express();
+
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? ["https://null.com", "https://null.com"] // accept requests only from these domains
+        : "http://localhost:5173", // accept requests from Vite dev server
+    credentials: true,
+  }),
+);
+app.use(helmet()); // set security HTTP headers
+app.use(express.json()); // parse JSON requests
+app.use(morgan("dev")); // log HTTP requests
+
+const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || "development";
+const __dirname = path.resolve();
+const DIST_PATH = path.join(__dirname, "dist-frontend");
+
+// normalize trailing slashes
+app.use((req, res, next) => {
+  if (
+    req.path.length > 1 &&
+    req.path.endsWith("/") &&
+    !req.path.startsWith("/api")
+  ) {
+    return res.redirect(301, req.path.slice(0, -1));
+  }
+  next();
+});
+
+if (NODE_ENV === "production") {
+  app.use(express.static(DIST_PATH));
+
+  // handle React SPA routing
+  app.get("*", (_req, res) => {
+    res.sendFile(path.join(DIST_PATH, "index.html"));
+  });
+} else {
+  console.log("🚧 Running in dev mode... 🚧");
+}
+
+// test API endpoint
+app.get("/api/hello", (_req, res) => {
+  res.json({ message: "Hello from Express backend!" });
+});
+
+app.listen(PORT, () => {
+  initDB();
+  console.log(`Server running on http://localhost:${PORT}`);
+});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3000,
+    port: 5173,
     // host: "0.0.0.0",
     server:
       mode === "development"
