41aaf1d861c5c1c13652cd3f13b9e757e946234f
isMaya404 <franciajeremiah10@gmail.com>
2025-10-23 13:57:50 +0800

    style(hero): add layer text

--- frontend/src/pages/home/components/Hero.tsx
+++ frontend/src/pages/home/components/Hero.tsx
@@ -5,7 +5,7 @@ import TBC_1 from "@/assets/thecozybud/TCB_1.png";
 const Hero: React.FC = () => {
   return (
     <section
-      className="w-full lg:h-[550px] 2xl:h-[650px] flex items-center justify-center pb-10"
+      className="flex w-full items-center justify-center pb-10 lg:h-[550px] 2xl:h-[650px]"
       aria-label="Hero"
     >
       <HeroImage />
@@ -23,17 +23,27 @@ export const HeroImage = () => {
   }, []);
 
   return (
-    <>
-      {!loaded && <Skeleton className="w-full h-full bg-card rounded-none" />}
+    <div className="relative h-full w-full">
+      {!loaded && <Skeleton className="h-full w-full rounded-none bg-card" />}
       {loaded && (
         <img
           src={TBC_1}
           alt=""
-          className="lg:object-cover w-full h-full transition-opacity duration-300 opacity-100"
+          className="h-full w-full scale-130 opacity-100 transition-opacity duration-300 lg:scale-100 lg:object-cover"
           loading="eager"
         />
       )}
-    </>
+
+      <div className="absolute top-[40%]  left-[7%]  flex flex-col gap-1 text-white md:top-[60%] md:left[15%] lg:gap-6">
+        <h1 className="font-ivy-ora-display font-bold text-destructive md:text-2xl lg:text-4xl">
+          BLOSSOMING ELEGANCE
+        </h1>
+
+        <p className="font-ivy-ora-display text-sm max-w-[230px] md:text-2xl md:max-w-[390px] lg:text-3xl lg:max-w-[490px]">
+          Fresh, handcrafted arrangements designed to elevate everyday moments.
+        </p>
+      </div>
+    </div>
   );
 };
 
