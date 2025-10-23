c08e1ecab15825152283a82dad5f808c241eb431
isMaya404 <franciajeremiah10@gmail.com>
2025-10-23 12:33:23 +0800

    chore(config): add host on vite config to access the dev server using mobile phones

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
-    // host: "0.0.0.0",
+    host: "0.0.0.0",
     server:
       mode === "development"
         ? {
