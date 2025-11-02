45f53d2e56c6419cca281c0300516a749adf15eb
isMaya404 <franciajeremiah10@gmail.com>
2025-11-02 08:21:34 +0800

    style: add padding on all page components  except home

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,10 +1,18 @@
 import { Outlet } from "react-router";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import { cn } from "@/lib/utils/cn";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 function Root() {
+  const path = window.location.pathname;
+  const isMediumScreenAndBelow = useMediaQuery("(max-width: 1023px)");
+  const ptVal = isMediumScreenAndBelow ? "pt-15" : "pt-20";
+
   return (
-    <div className="flex flex-col min-h-screen">
+    <div
+      className={cn(path !== "/home" && ptVal, "flex flex-col min-h-screen")}
+    >
       <NavBar />
       <Outlet />
       <Footer />
