38c4cdfdc5cdb699a611fc0b2697235680cd1668
isMaya404 <franciajeremiah10@gmail.com>
2025-11-02 10:15:12 +0800

    add: workaround Suspense integration for Tanstack Query's useSuspenseQuery

---
+++ frontend/src/components/PersistSuspense.tsx
@@ -0,0 +1,35 @@
+import { Suspense, type ReactNode } from "react";
+import { useIsRestoring } from "@tanstack/react-query";
+
+// add: PersistSuspense - Suspense integration for Tanstack Query's
+// useSuspenseQuery hook
+
+// Workaround for using useSuspenseQuery with CSR and persisted cache (localStorage, db
+// cache, etc.) which doesn't work with React's Suspense.
+
+// https://github.com/TanStack/query/issues/8400
+
+type PersistGateProps = {
+  children: ReactNode;
+  fallback?: ReactNode;
+};
+
+export function PersistGate({ children, fallback = null }: PersistGateProps) {
+  const isRestoring = useIsRestoring();
+  return isRestoring ? fallback : children;
+}
+
+type PersistSuspenseProps = {
+  children: ReactNode;
+  fallback: ReactNode;
+};
+
+const PersistSuspense = ({ children, fallback }: PersistSuspenseProps) => {
+  return (
+    <Suspense fallback={fallback}>
+      <PersistGate fallback={fallback}>{children}</PersistGate>
+    </Suspense>
+  );
+};
+
+export default PersistSuspense;
