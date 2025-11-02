6a329f15a13e2d1ff3a5104a6dfa9288462a0833
isMaya404 <franciajeremiah10@gmail.com>
2025-11-02 16:49:01 +0800

    fix: wrong pathname

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,17 +1,20 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import { cn } from "@/lib/utils/cn";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 function Root() {
-  const path = window.location.pathname;
+  const location = useLocation();
   const isMediumScreenAndBelow = useMediaQuery("(max-width: 1023px)");
   const ptVal = isMediumScreenAndBelow ? "pt-15" : "pt-20";
 
   return (
     <div
-      className={cn(path !== "/home" && ptVal, "flex flex-col min-h-screen")}
+      className={cn(
+        location.pathname !== "/" && ptVal,
+        "flex flex-col min-h-screen",
+      )}
     >
       <NavBar />
       <Outlet />
