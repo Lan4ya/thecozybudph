803dd6e84aba5f4e1808ca7f1347b3173d979dee
isMaya404 <franciajeremiah10@gmail.com>
2025-11-08 11:26:47 +0800

    style(toast): change text overflows, ellipsis, and structure

--- frontend/src/providers/ToastProvider.tsx
+++ frontend/src/providers/ToastProvider.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils/cn";
 import { AnimatePresence, motion } from "framer-motion";
 import React, { createContext, useContext, useState, useCallback } from "react";
 
@@ -50,23 +51,25 @@ export const ToastProvider: React.FC<{ children: React.ReactNode }> = ({
   return (
     <ToastContext.Provider value={{ addToast }}>
       {children}
-      <div className="fixed top-5 right-5 z-[9999] flex flex-col gap-3">
+
+      <div className="text-center fixed top-5 right-5 z-9999 flex flex-col gap-3">
         <AnimatePresence>
           {toasts.map((t) => (
             <motion.div
               key={t.id}
+              layout="position"
               initial={{ opacity: 0, y: 20, scale: 0.9 }}
               animate={{ opacity: 1, y: 0, scale: 1 }}
               exit={{ opacity: 0, y: -20, scale: 0.9 }}
               transition={{ duration: 0.25 }}
-              className={`
-                rounded-lg px-4 py-3 shadow-lg text-white font-medium
-                ${t.type === "success" ? "bg-green-600" : ""}
-                ${t.type === "error" ? "bg-red-600" : ""}
-                ${t.type === "info" ? "bg-blue-600" : ""}
-              `}
+              className={cn(
+                "w-72 rounded-lg px-4 py-3 shadow-md font-medium text-white",
+                t.type === "success" && "bg-green-600",
+                t.type === "error" && "bg-red-600",
+                t.type === "info" && "bg-white text-black",
+              )}
             >
-              {t.message}
+              <p className="overflow-hidden overflow-ellipsis">{t.message}</p>
             </motion.div>
           ))}
         </AnimatePresence>
