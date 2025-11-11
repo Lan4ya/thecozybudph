41ca3be2be6685c8d5f8805be8da73e607d89fd9
isMaya404 <franciajeremiah10@gmail.com>
2025-11-11 21:42:24 +0800

    add: scroll to top on every route change (since react router doesnt reset scroll pos when navigating pages)

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -2,6 +2,7 @@ import { Outlet, useLocation } from "react-router";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import { cn } from "@/lib/utils/cn";
+import { ScrollToTop } from "@/components/ScrollTop";
 
 function Root() {
   const location = useLocation();
@@ -13,6 +14,7 @@ function Root() {
         location.pathname !== "/" && "pt-15 lg:pt-20",
       )}
     >
+      <ScrollToTop />
       <NavBar />
       <Outlet />
       <Footer />
