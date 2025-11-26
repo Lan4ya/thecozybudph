3e42448694efca6e9040c960dd2696ef9a75081f
isMaya404 <franciajeremiah10@gmail.com>
2025-11-26 23:31:12 +0800

    style: scale the card instead instead of image

--- frontend/src/components/products/ProductCard.tsx
+++ frontend/src/components/products/ProductCard.tsx
@@ -19,12 +19,12 @@ const ProductCard = ({ productId, name, imageUrl, price }: Props) => {
 
   return (
     <div onClick={handleClick}>
-      <div className="group bg-card select-none text-card-foreground rounded-lg border border-border/30 shadow-sm hover:shadow-md transition-all duration-200 overflow-hidden cursor-pointer active:scale-[0.99] active:shadow-lg">
+      <div className="hover:scale-102 bg-card select-none text-card-foreground rounded-lg border border-border/30 shadow-sm hover:shadow-md transition-all duration-200 overflow-hidden cursor-pointer active:scale-[0.99] active:shadow-lg">
         <div className="relative overflow-hidden">
           <ProductImage
             loading="lazy"
             src={imageUrl}
-            className="pointer-events-none aspect-square group-hover:scale-105 transition-transform duration-200 rounded-b-none"
+            className="pointer-events-none aspect-square  transition-transform duration-200 rounded-b-none"
           />
         </div>
 
