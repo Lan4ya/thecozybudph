e8dfb151e744d01aa6068e8b37c69720eb9bcb54
isMaya404 <franciajeremiah10@gmail.com>
2025-12-29 17:01:02 +0800

    refactor: abstract repeated and sane middlewares into defaultMiddleware function

---
+++ backend/supabase/functions/_shared/middlewares/defaultMiddleware.ts
@@ -0,0 +1,40 @@
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { secureHeaders } from "hono/secure-headers";
+import { rateLimiter } from "hono-rate-limiter";
+import type { Hono } from "hono";
+import { AppEnv } from "../types.d.ts";
+
+// Apply sane default middlewares on all edge functions
+export function applyDefaultMiddlewares(app: Hono<AppEnv>) {
+  app.use(logger());
+
+  app.use(
+    "*",
+    cors({
+      origin: [
+        "http://127.0.0.1:5173", // dev only
+        "https://thecozybudph.com",
+        "https://thecozybudph.vercel.app",
+      ],
+      credentials: true,
+      maxAge: 86400,
+    }),
+  );
+
+  app.use(secureHeaders());
+
+  app.use(
+    "*",
+    rateLimiter({
+      windowMs: 15 * 60 * 1000,
+      limit: 200,
+      standardHeaders: "draft-6",
+      keyGenerator: (c) =>
+        c.req.header("cf-connecting-ip") ||
+        c.req.header("x-forwarded-for") ||
+        c.req.header("x-real-ip") ||
+        "anonymous",
+    }),
+  );
+}
--- backend/supabase/functions/product/index.ts
+++ backend/supabase/functions/product/index.ts
@@ -1,45 +1,12 @@
-
 import { Hono } from "hono";
 import product from "./product-routes.ts";
 import { handleError } from "@shared/middlewares/errorHandler.ts";
-import { cors } from "hono/cors";
-import { logger } from "hono/logger";
-import { rateLimiter } from "hono-rate-limiter";
-import { secureHeaders } from "hono/secure-headers";
 import { AppEnv } from "@shared/types.d.ts";
+import { applyDefaultMiddlewares } from "@shared/middlewares/defaultMiddleware.ts";
 
 const app = new Hono<AppEnv>().basePath("product");
 
-app.use(logger());
-app.use(
-  "*",
-  cors({
-    origin: [
-      "http://127.0.0.1:5173", // dev
-      "https://thecozybudph.com",
-      "https://thecozybudph.vercel.app",
-    ],
-    credentials: true,
-    maxAge: 86400,
-  }),
-);
-app.use(secureHeaders());
-app.use(
-  "*",
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15mins
-    limit: 200, // Limit each IP to N requests per `window`
-    standardHeaders: "draft-6",
-    keyGenerator: (c) => {
-      return (
-        c.req.header("cf-connecting-ip") ||
-        c.req.header("x-forwarded-for") ||
-        c.req.header("x-real-ip") ||
-        "anonymous"
-      );
-    },
-  }),
-);
+applyDefaultMiddlewares(app);
 
 app.route("/", product);
 
